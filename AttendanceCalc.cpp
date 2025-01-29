c98a0bcf60bd7fb5f0ab2b8497a4f6372aca0115
Atharv Sheersh Chaturvedi <144057375+AtharvSChaturvedi@users.noreply.github.com>
2025-01-29 19:50:01 +0530

    Add files via upload

---
+++ AttendanceCalc.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int days, present, days_required=0;
+    float percentage_wanted, current_percentage;
+    cout<<"Enter No. of Days: ";
+    cin>>days;
+    cout<<"Enter No. of Present: ";
+    cin>>present;
+
+    current_percentage=present/days;
+
+    cout<<"Percentage wanted: ";
+    cin>>percentage_wanted;
+
+    if (percentage_wanted>=100){
+        cout<<"INVALID!";
+    }
+    if (percentage_wanted<=current_percentage){
+        cout<<"Already achieved";
+    }
+    else {
+        while((present/(float)days)*100 < percentage_wanted){
+            present++;
+            days++;
+            days_required++;
+        }
+    }
+
+    cout<<"Number of days required: "<<days_required;
+}
